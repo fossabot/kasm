aed7687a812c3c03728dfade3905fbb38854d41f
Kyle Escobar <kslrtips@gmail.com>
2020-10-08 00:34:41 -0400

    Add class file object

---
+++ core/src/main/kotlin/org/spectralpowered/kasm/core/ClassFile.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2020 Kyle Escobar
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.spectralpowered.kasm.core
+
+import org.objectweb.asm.ClassVisitor
+import org.objectweb.asm.Opcodes.ASM9
+import org.objectweb.asm.Type
+
+/**
+ * Represents a Java class file's bytecode model.
+ *
+ * @property pool [ClassPool] that this class belongs in
+ * @constructor
+ */
+class ClassFile(val pool: ClassPool) : ClassVisitor(ASM9) {
+
+    /**
+     * The class name.
+     */
+    lateinit var name: String
+
+    /**
+     * The bit-packed access flags.
+     */
+    var accessFlags: Int = 0
+
+    /**
+     * JVM version this class was compiled with.
+     */
+    var version: Int = 0
+
+    /**
+     * The Name of the extended class or parent class.
+     */
+    lateinit var superClass: String
+
+    /**
+     * The class names that this implements
+     */
+    var interfaces = mutableListOf<String>()
+
+    /**
+     * The ASM object [Type].
+     */
+    val type: Type by lazy { Type.getObjectType(name) }
+
+    override fun toString(): String {
+        return name
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "kasm"
 
+include(":core")
